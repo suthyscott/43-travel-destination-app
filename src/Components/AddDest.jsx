f59149783879714b83a3a63651a73525435f7625
suthyscott <sutherlandscott21@gmail.com>
2023-11-30 16:18:00 -0700

    finished form submit and brought in a notification library

--- src/Components/AddDest.jsx
+++ src/Components/AddDest.jsx
@@ -2,22 +2,30 @@ import "./AddDest.css"
 import { useState } from "react"
 import { Formik } from "formik"
 import axios from "axios"
+import { useNavigate } from "react-router-dom"
+import { toast, ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 const AddDest = ({ addDestination }) => {
-    // const [destName, setDestName] = useState("")
-    // const [notes, setNotes] = useState("")
-    // const [imageURL, setImageURL] = useState("")
-    // const [international, setInternational] = useState(false)
+    const navigate = useNavigate()
 
-    const submitHandler = values => {
-        console.log(values)
-        // e.preventDefault()
-        // const bodyObj = {
-        //     name: destName,
-        //     notes,
-        //     imageURL,
-        //     international
-        // }
+    const submitHandler = (values, funcs) => {
+        console.log(funcs)
+        const { resetForm } = funcs
+
+        axios
+            .post(
+                `http://localhost:4545/api/destinations?apiKey=${process.env.REACT_APP_API_KEY}`,
+                values
+            )
+            .then(res => {
+                console.log(res.data)
+                // navigate(`/details/${res.data.id}`)
+                // alert('Your destination was added.')
+                toast("Your Destination was added!")
+                resetForm()
+            })
+            .catch(err => console.log(err))
     }
 
     const initialValues = {
@@ -28,43 +36,55 @@ const AddDest = ({ addDestination }) => {
     }
 
     return (
-        <Formik initialValues={initialValues} onSubmit={submitHandler}>
-            {({ values, handleChange, handleSubmit }) => {
-                // const { values, handleChange, handleSubmit } = formikParams
-                return (
-                    <form
-                        id="add-dest-container"
-                        onSubmit={handleSubmit}
-                    >
-                        <input
-                            placeholder="name"
-                            onChange={handleChange}
-                            name='name'
-                            value={values.name}
-                        />
-                        <input
-                            placeholder="notes about your destination"
-                            onChange={handleChange}
-                            name='notes'
-                            value={values.notes}
-                        />
-                        <input
-                            placeholder="Image URL"
-                            onChange={handleChange}
-                            value={values.imageURL}
-                            name="imageURL"
-                        />
-                        <input
-                            type="checkbox"
-                            checked={values.international}
-                            name="international"
-                            onChange={handleChange}
-                        />
-                        <button type="submit">Add Destination</button>
-                    </form>
-                )
-            }}
-        </Formik>
+        <div>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
+
+            <Formik initialValues={initialValues} onSubmit={submitHandler}>
+                {({ values, handleChange, handleSubmit }) => {
+                    // const { values, handleChange, handleSubmit } = formikParams
+                    return (
+                        <form id="add-dest-container" onSubmit={handleSubmit}>
+                            <input
+                                placeholder="name"
+                                onChange={handleChange}
+                                name="name"
+                                value={values.name}
+                            />
+                            <input
+                                placeholder="notes about your destination"
+                                onChange={handleChange}
+                                name="notes"
+                                value={values.notes}
+                            />
+                            <input
+                                placeholder="Image URL"
+                                onChange={handleChange}
+                                value={values.imageURL}
+                                name="imageURL"
+                            />
+                            <input
+                                type="checkbox"
+                                checked={values.international}
+                                name="international"
+                                onChange={handleChange}
+                            />
+                            <button type="submit">Add Destination</button>
+                        </form>
+                    )
+                }}
+            </Formik>
+        </div>
     )
 }
 
