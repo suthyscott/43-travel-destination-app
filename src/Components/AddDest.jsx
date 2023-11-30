5de58a3cbb7141f5d478914665fc740108c2a1ff
suthyscott <sutherlandscott21@gmail.com>
2023-11-30 09:42:52 -0700

    formik

--- src/Components/AddDest.jsx
+++ src/Components/AddDest.jsx
@@ -1,41 +1,70 @@
 import "./AddDest.css"
 import { useState } from "react"
+import { Formik } from "formik"
+import axios from "axios"
 
 const AddDest = ({ addDestination }) => {
-    const [destName, setDestName] = useState("")
-    const [notes, setNotes] = useState("")
-    const [imageURL, setImageURL] = useState("")
-    const [international, setInternational] = useState(false)
+    // const [destName, setDestName] = useState("")
+    // const [notes, setNotes] = useState("")
+    // const [imageURL, setImageURL] = useState("")
+    // const [international, setInternational] = useState(false)
 
-    const submitHandler = e => {
-        e.preventDefault()
-        const bodyObj = {
-            name: destName,
-            notes,
-            imageURL,
-            international
-        }
-        addDestination(bodyObj)
+    const submitHandler = values => {
+        console.log(values)
+        // e.preventDefault()
+        // const bodyObj = {
+        //     name: destName,
+        //     notes,
+        //     imageURL,
+        //     international
+        // }
+    }
+
+    const initialValues = {
+        name: "",
+        notes: "",
+        international: false,
+        imageURL: ""
     }
 
     return (
-        <form id="add-dest-container" onSubmit={e => submitHandler(e)}>
-            <input placeholder="name" onChange={e => setDestName(e.target.value)} />
-            <input
-                placeholder="notes about your destination"
-                onChange={e => setNotes(e.target.value)}
-            />
-            <input
-                placeholder="Image URL"
-                onChange={e => setImageURL(e.target.value)}
-            />
-            <input
-                type="checkbox"
-                checked={international}
-                onChange={() => setInternational(!international)}
-            />
-            <button>Add Destination</button>
-        </form>
+        <Formik initialValues={initialValues} onSubmit={submitHandler}>
+            {({ values, handleChange, handleSubmit }) => {
+                // const { values, handleChange, handleSubmit } = formikParams
+                return (
+                    <form
+                        id="add-dest-container"
+                        onSubmit={handleSubmit}
+                    >
+                        <input
+                            placeholder="name"
+                            onChange={handleChange}
+                            name='name'
+                            value={values.name}
+                        />
+                        <input
+                            placeholder="notes about your destination"
+                            onChange={handleChange}
+                            name='notes'
+                            value={values.notes}
+                        />
+                        <input
+                            placeholder="Image URL"
+                            onChange={handleChange}
+                            value={values.imageURL}
+                            name="imageURL"
+                        />
+                        <input
+                            type="checkbox"
+                            checked={values.international}
+                            name="international"
+                            onChange={handleChange}
+                        />
+                        <button type="submit">Add Destination</button>
+                    </form>
+                )
+            }}
+        </Formik>
     )
 }
 
